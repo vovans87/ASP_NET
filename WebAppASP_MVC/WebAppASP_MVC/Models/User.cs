fe64da802d3f55e2083bfb3ba68d80e2ecf4197d
vovans87 <excellentwebmaster@rambler.ru>
2016-03-25 15:41:25 +0300

    commit

---
+++ WebAppASP_MVC/WebAppASP_MVC/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppASP_MVC.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public string password { get; set; }
+
+        public int age { get; set; }
+
+        public List<string> country { get; set; }
+    }
+}
